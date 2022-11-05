77502c2a409177e45533d2b86e2f5b964dce7a70
Vinícius Rodrigues <viniciusrf2@al.insper.edu.br>
2022-11-05 11:45:45 -0300

    Adiciona função 'sorteia_questao'

--- fortuna_dessoft.py
+++ fortuna_dessoft.py
@@ -1,10 +1,17 @@
 #### BEM-VINDO AO FORTUNA DESSOFT - BY SAMUEL JABES E VINICIUS RODRIGUES ####
 
-def transforma_base(questoes):
+from random import randint
+
+def transforma_base(lista_questoes):
     questoes_por_nivel = {}
-    for questao in questoes:
+    for questao in lista_questoes:
         if questao['nivel'] not in questoes_por_nivel:
             questoes_por_nivel[questao['nivel']] = [questao]
         else:
             questoes_por_nivel[questao['nivel']].append(questao)
     return questoes_por_nivel
+
+def sorteia_questao(lista_questoes, nivel):
+    numero_questao = randint(0, len(lista_questoes[nivel])-1)
+    questao_sorteada = lista_questoes[nivel][numero_questao]
+    return questao_sorteada
